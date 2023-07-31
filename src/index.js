85477e302ce54ba692ba9d599a59d442e75cb16b
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-07-31 22:39:23 +0300

    fix 404

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { store } from './redux/store';
 import App from './components/App';
-import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
